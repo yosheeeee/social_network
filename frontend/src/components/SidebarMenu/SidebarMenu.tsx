6ab8f0d02d99476cf2759c17088930c9dda0b2ac
yosheeeee <evantipin@edu.hse.ru>
2024-01-24 00:22:24 +0500

    add menu component

---
+++ frontend/src/components/SidebarMenu/SidebarMenu.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./SidebarMenu.scss"
+import menu_items from "./menu_items";
+export interface SidebarMenuProps{
+
+}
+
+export interface SidebarMenuItemProps{
+    icon_name: string,
+    title: string,
+    link?:string,
+}
+
+export  function SidebarMenu(props: SidebarMenuProps){
+    return (
+        <div className="sidebar_menu">
+            {menu_items.map(item => SidebarMenuItem(item))}
+        </div>
+    )
+}
+
+function SidebarMenuItem(props:SidebarMenuItemProps){
+    const {icon_name, title, link} = props
+    return (
+            <div className="menu_item">
+                <i className={"fa-solid "+icon_name}></i>
+                <a href={link ? link : "#"}>{title}</a>
+            </div>
+            )
+}
---
+++ frontend/src/components/SidebarMenu/menu_items.ts
@@ -0,0 +1,26 @@
+import {SidebarMenuItemProps} from "./SidebarMenu";
+
+let menu_items : SidebarMenuItemProps[] = [
+    {
+        icon_name: "fa-house",
+        title: 'Главная',
+    },
+    {
+        icon_name: 'fa-bell',
+        title: 'Уведомления'
+    },
+    {
+        icon_name: 'fa-comment',
+        title: 'Сообщения'
+    },
+    {
+        icon_name: 'fa-user',
+        title: 'Профиль'
+    },
+    {
+        icon_name: 'fa-gear',
+        title: "Настройки"
+    }
+]
+
+export default menu_items
