06ed502dbb8f5db60819e96a138af44cada84523
yosheeeee <evantipin@edu.hse.ru>
2024-04-03 01:00:56 +0500

    add slider and images to frontend

---
+++ frontend/src/components/Slider/slider.tsx
@@ -0,0 +1,42 @@
+import {Children, ReactNode, useState} from "react";
+import "./slider.scss"
+
+export default function Slider({children, elementsOnPage}: { children: ReactNode, elementsOnPage: number }) {
+    const [currentElem, setCurrentElem] = useState(0)
+    const childrenLength = Children.count(children)
+
+    function moveLeft() {
+        setCurrentElem(prevState => prevState >= 1 ? prevState - 1 : childrenLength - elementsOnPage)
+    }
+
+    function moveRight() {
+        setCurrentElem(prevState => prevState < childrenLength - elementsOnPage ? prevState + 1 : 0)
+    }
+
+
+    return (
+        <div className="slider">
+            <div className="slider-content-container">
+                <div className={"slider-content "+'elements--'+elementsOnPage}
+                     style={{transform: `translateX(${-100 / elementsOnPage * currentElem}%)`}}>
+                    {children}
+                </div>
+            </div>
+            {
+                childrenLength > elementsOnPage ?
+                    <>
+                        <button className="slider-btn left button rounded"
+                                onClick={moveLeft}><i className="fa-solid fa-chevron-left"></i></button>
+
+                        <button
+                            onClick={moveRight}
+                            className="slider-btn right button rounded"><i className="fa-solid fa-chevron-right"></i>
+                        </button>
+                    </>
+                    :
+                    <>
+                    </>
+            }
+        </div>
+    )
+}
--- frontend/src/pages/UserPage/userPage.tsx
+++ frontend/src/pages/UserPage/userPage.tsx
@@ -7,11 +7,11 @@ import UserImage from "../../components/UserImage";
 import useUserPage, {UserData, UserPageParams, UserPost} from "../../hooks/useUserPage";
 import PostStats from "../../components/post-stats/postStats";
 import {Outlet} from "react-router-dom";
-
-
+import axios from "axios";
+import Slider from "../../components/Slider/slider";
 
 export default function UserPage() {
-    let {loading , userData  , userImageSrc , PostForm , HeaderButton , userPosts} = useUserPage()
+    let {loading, userData, userImageSrc, PostForm, HeaderButton, userPosts} = useUserPage()
 
 
     return (
@@ -19,10 +19,11 @@ export default function UserPage() {
             {loading && <Loader/>}
             {!loading && <>
                 <div className="user-header">
-                    <img src={UserBackground} className="header-background"/>
+                    <img src={UserBackground}
+                         className="header-background"/>
                     <div className="header-data">
                         <div className="user-data">
-                            <UserImage file_src={BACKEND_PATH + '/static'+userImageSrc}/>
+                            <UserImage file_src={BACKEND_PATH + '/static' + userImageSrc}/>
                             <p>{userData.user_name}</p>
                             <p className="color-blue">@{userData.user_login}</p>
                             <p className='user-sub'>
@@ -39,40 +40,62 @@ export default function UserPage() {
 
                 <div id="user-posts">
                     <h2>Записи пользователя:</h2>
-
-
                     {userPosts.length == 0 ? <h3>Записи отсутстуют</h3> :
-
-                        userPosts.map(post => {
-                                let date = new Date(post.post_date)
-                                let options = {
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric',
-                                    weekday: 'long',
-                                    timezone: 'UTC',
-                                    hour: 'numeric',
-                                    minute: 'numeric',
-                                };
-
-                                return (
-                                    <div className='user-post'>
-                                        <div className="post-date">{`${date.getHours()}:${date.getMinutes()}`}, {date.toLocaleDateString("ru", {
-                                            year: 'numeric',
-                                            month: 'long',
-                                            day: 'numeric',
-                                            weekday: 'long',
-                                        })}</div>
-                                        <div className='post-content'
-                                             dangerouslySetInnerHTML={{__html: post.content}}></div>
-                                        <PostStats {...post}/>
-                                    </div>
-                                )
-                            }
-                        )
+                        userPosts.map(post => <PostTemplate {...post}/>)
                     }
                 </div>
             </>}
+        </div>
+    )
+}
+
+function PostTemplate(post: UserPost) {
+    let date = new Date(post.post_date)
+    let options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        weekday: 'long',
+        timezone: 'UTC',
+        hour: 'numeric',
+        minute: 'numeric',
+    };
+    const [postImagesSrc, setPostImagesSrc] = useState<{ file_src: string }[]>([])
+
+    useEffect(() => {
+        axios.get(BACKEND_PATH + '/post/images/' + post.id)
+            .then(res => {
+                console.log(res.data)
+                return res.data.images
+            })
+            .then(images => setPostImagesSrc(images))
+    }, [post]);
+
+    return (
+        <div className='user-post'>
+            <div className="post-date">{`${date.getHours()}:${date.getMinutes()}`}, {date.toLocaleDateString("ru", {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                weekday: 'long',
+            })}</div>
+            <div className='post-content'
+                 dangerouslySetInnerHTML={{__html: post.content}}></div>
+            {
+                postImagesSrc.length ?
+
+                    <div className="post-images">
+                        <Slider elementsOnPage={3}>
+                            {postImagesSrc.map(image => <div><img src={BACKEND_PATH + '/static/' + image.file_src}
+                                                                  alt="post-image"/></div>)}
+                        </Slider>
+                    </div>
+                    :
+                    <>
+                    </>
+
+            }
+            <PostStats {...post}/>
         </div>
     )
 }
